1347e70cb4d365401b6dd3c45b58540d8438a205
Santosh A <asantosh.king@gmail.com>
2015-04-04 13:28:50 +0530

    UPdate

--- CProgram/README.md
+++
@@ -1,5 +0,0 @@
-Programming
-=========
-
-Contains all programming related doc's and example code.
-Programming languages : "C", "Python".
--- CProgram/malloc/basic_malloc/malloc.h
+++
@@ -1,8 +0,0 @@
-#ifndef __MALLOC_H__
-#define __MALLOC_H__
-
-void *m_malloc(size_t size);
-void *m_calloc(size_t mem, size_t size);
-void m_free(void *ptr);
-
-#endif /*__MALLOC_H__*/
